b0c502a0426669fc713433106f3fd7aec23512d5
IDOKO EMMANUEL <pidox31102002@gmail.com>
2023-03-14 11:37:22 +0100

    updates

--- chpt4/4_27.c
+++ chpt4/4_27.c
@@ -3,16 +3,16 @@
 
 int main(){
     unsigned int i,j,k;
-    int x,y,z;
+    // int x,y,z;
 
     for(i=1; i<= 500; i++){
         for(j=1;j<=500; j++){
             for(k=1;k<=500;k++){
-                x = pow(i,2);
-                y = pow(j,2);
-                z = pow(k,2);
+                // x = pow(i,2);
+                // y = pow(j,2);
+                // z = pow(k,2);
 
-                if ( x + y == z){
+                if ( pow(i,2) + pow(j,2) == pow(k,2)){
                     printf("%u, %u and %u are pythagoran triplets \n", i, j, k);
                 } else{
                     continue;
